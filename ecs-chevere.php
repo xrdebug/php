28bdd99413f18049c6082791e5acd3d7e91aebe0
Rodolfo Berrios <inbox@rodolfoberrios.com>
2021-01-12 15:00:56 -0300

    drops conflicting setlist

--- ecs-chevere.php
+++ ecs-chevere.php
@@ -11,18 +11,34 @@
 
 declare(strict_types=1);
 
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
 use PhpCsFixer\Fixer\ClassNotation\ClassAttributesSeparationFixer;
+use PhpCsFixer\Fixer\ClassNotation\ProtectedToPrivateFixer;
 use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
 use PhpCsFixer\Fixer\Comment\SingleLineCommentStyleFixer;
 use PhpCsFixer\Fixer\ControlStructure\IncludeFixer;
+use PhpCsFixer\Fixer\ControlStructure\NoUnneededControlParenthesesFixer;
+use PhpCsFixer\Fixer\ControlStructure\NoUnneededCurlyBracesFixer;
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\Fixer\Import\OrderedImportsFixer;
 use PhpCsFixer\Fixer\LanguageConstruct\CombineConsecutiveUnsetsFixer;
 use PhpCsFixer\Fixer\Operator\ObjectOperatorWithoutWhitespaceFixer;
+use PhpCsFixer\Fixer\ReturnNotation\ReturnAssignmentFixer;
 use PhpCsFixer\Fixer\Semicolon\MultilineWhitespaceBeforeSemicolonsFixer;
+use PhpCsFixer\Fixer\Semicolon\NoEmptyStatementFixer;
 use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
 use PhpCsFixer\Fixer\Whitespace\BlankLineBeforeStatementFixer;
 use PhpCsFixer\Fixer\Whitespace\CompactNullableTypehintFixer;
 use PhpCsFixer\Fixer\Whitespace\NoExtraBlankLinesFixer;
+use SlevomatCodingStandard\Sniffs\ControlStructures\RequireShortTernaryOperatorSniff;
+use SlevomatCodingStandard\Sniffs\Functions\UnusedInheritedVariablePassedToClosureSniff;
+use SlevomatCodingStandard\Sniffs\Operators\RequireCombinedAssignmentOperatorSniff;
+use SlevomatCodingStandard\Sniffs\PHP\DisallowDirectMagicInvokeCallSniff;
+use SlevomatCodingStandard\Sniffs\PHP\UselessSemicolonSniff;
+use SlevomatCodingStandard\Sniffs\Variables\UnusedVariableSniff;
+use SlevomatCodingStandard\Sniffs\Variables\UselessVariableSniff;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\CodingStandard\Fixer\Commenting\ParamReturnAndVarTagMalformsFixer;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
@@ -31,7 +47,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $header = file_get_contents(__DIR__ . '/.header');
     $parameters->set(Option::SETS, [
         SetList::PSR_12,
-        SetList::CLEAN_CODE,
         SetList::COMMON,
     ]);
     $services = $containerConfigurator->services();
@@ -49,6 +64,25 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(SingleLineCommentStyleFixer::class);
     $services->set(IncludeFixer::class);
     $services->set(ObjectOperatorWithoutWhitespaceFixer::class);
+    $services->set(DisallowDirectMagicInvokeCallSniff::class);
+    $services->set(ParamReturnAndVarTagMalformsFixer::class);
+    $services->set(UnusedVariableSniff::class);
+    $services->set(UselessVariableSniff::class);
+    $services->set(UnusedInheritedVariablePassedToClosureSniff::class);
+    $services->set(UselessSemicolonSniff::class);
+    $services->set(ArraySyntaxFixer::class)
+        ->call('configure', [[
+            'syntax' => 'short',
+        ]]);
+    $services->set(NoUnusedImportsFixer::class);
+    $services->set(OrderedImportsFixer::class);
+    $services->set(NoEmptyStatementFixer::class);
+    $services->set(ProtectedToPrivateFixer::class);
+    $services->set(NoUnneededControlParenthesesFixer::class);
+    $services->set(NoUnneededCurlyBracesFixer::class);
+    $services->set(ReturnAssignmentFixer::class);
+    $services->set(RequireShortTernaryOperatorSniff::class);
+    $services->set(RequireCombinedAssignmentOperatorSniff::class);
     $services->set(NoExtraBlankLinesFixer::class)
         ->call('configure', [[
             'curly_brace_block',
