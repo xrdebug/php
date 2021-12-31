74157229f19936e892af06f8680ab4ae00ac13df
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2021-12-31 19:56:54 -0300

    improves demo

--- demo.php
+++ demo.php
@@ -11,17 +11,75 @@
 
 declare(strict_types=1);
 
+use function Chevere\Components\Message\message;
+
 require __DIR__ . '/vendor/autoload.php';
 
-xr(['Hola, mundo!', new stdClass()]);
-sleep(1);
-xr(f: '🤔');
-xr(t: 'Win');
-sleep(1);
+xr(greet: 'Hola, mundo!', t: 'hello-world', f: '😊');
+sleep(2);
 xr(
-    getrusage(),
-    f: '😎',
-    t: 'Epic win!'
+    explain: [
+        1 => 'chevere/xr is a debugger which',
+        2 => 'runs a message server!'
+    ],
+    t: 'hello-world',
+    f: '🤓'
 );
-sleep(1);
-xr($_SERVER);
+sleep(4);
+xr(
+    message('Did you heard about %inspiration%?')
+        ->code('%inspiration%', 'spatie/ray'),
+    t: 'hello-world',
+    f: '🕶'
+);
+sleep(4);
+xr(
+    tagline: new class() {
+        public array $ohhh = [
+            'XR is another take on the spatie/ray concept...',
+            '...But built on top of 👏ReactPHP.'
+        ];
+    },
+    t: '5 ☆ Jumbitos',
+    f: '🐘🐘🐘🐘🐘'
+);
+sleep(4);
+xr(
+    read: '^^^ This is how you use the XR debugger.',
+    t: 'how-to',
+);
+sleep(4);
+xr(
+    feat: 'Edit the title by clicking on "XR Session".',
+    t: 'how-to',
+    f: '✍️'
+);
+sleep(4);
+xr(
+    feat: 'Filter by clicking a topic (how-to button) or flair (👻 emoji).',
+    t: 'how-to',
+    f: '👻'
+);
+sleep(4);
+xr(
+    feat: 'Copy the file path by clicking on ' . basename(__FILE__) . ':' . (string) (__LINE__ + 2) . ' here below.',
+    t: 'how-to',
+    f: '👻'
+);
+sleep(4);
+xr(
+    feat: 'Dark/light mode follows your system preferences.',
+    t: 'how-to',
+    f: '🌚🌝'
+);
+sleep(4);
+xr(
+    feat: 'Open a new window/tab to spawn a new XR Session.',
+    t: 'how-to',
+    f: '🆕'
+);
+sleep(4);
+xr(
+    bye: 'Hope you enjoy XR!',
+    f: '😊'
+);
